230c61212511de3bb457f73eea7fb11632ccf1fd
Gromov Ilya <109763246+DMCHMB@users.noreply.github.com>
2023-07-16 15:16:18 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,11 +13,35 @@ public class Tracker {
         return item;
     }
 
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        int tmpSize = 0;
+        for (Item item : items) {
+            if (item != null) {
+                rsl[tmpSize] = item;
+                tmpSize++;
+            }
+        }
+        return Arrays.copyOf(rsl, tmpSize);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int tmpSize = 0;
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                rsl[tmpSize] = item;
+                tmpSize++;
+            }
+        }
+        return Arrays.copyOf(rsl, tmpSize);
+    }
+
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
-            if (item.getId() == id) {
+            if (item != null && item.getId() == id) {
                 rsl = item;
                 break;
             }
