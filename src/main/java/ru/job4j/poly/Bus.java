be08407d4603c5f3440b24f987cca9b6230d2630
Gromov Ilya <109763246+DMCHMB@users.noreply.github.com>
2023-07-18 19:39:16 +0300

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        System.out.println("Bus is driving");
+    }
+
+    @Override
+    public void passengers(int passengers) {
+        System.out.printf("I have %d passengers\n", passengers);
+    }
+
+    @Override
+    public double refill(double numberOfFuel) {
+        return numberOfFuel * 67;
+    }
+}
