0449127ba6bef111d0afb18ce02a3d294e3d34f6
Gromov Ilya <109763246+DMCHMB@users.noreply.github.com>
2023-07-20 23:07:41 +0300

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FactTest {
+
+    @Test
+    public void when5Then120() {
+        int n = 5;
+        int expected = 120;
+        int result = (new Fact()).calc(5);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Fact().calc(-1);
+                });
+        assertThat(exception.getMessage()).isEqualTo("N could not be less than 0");
+    }
+}
